41331afcfa50dcf1b10c2bee533bad275296b193
Larry Glenn <yrral.glenn@gmail.com>
2011-07-23 09:33:21 -0400

    Added .emacs.d

---
+++ home/.emacs.d/site-lisp/elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,16 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19934 12458
+;;;;;;  950965))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; findr-autoloads.el ends here
