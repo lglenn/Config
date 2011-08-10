7e4d99e72920a19d2caa6a9fa4fb2a8a4200c522
Larry Glenn <yrral.glenn@gmail.com>
2011-08-10 17:58:21 -0400

    Removed site-specific files, as well as elpa

--- home/.emacs.d/site-lisp/elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19934 12458
-;;;;;;  950965))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; findr-autoloads.el ends here
