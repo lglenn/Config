1987b2ed7993f76b03c2cc409022ebb40a0a30e3
Larry Glenn <yrral.glenn@gmail.com>
2017-12-02 13:53:23 -0500

    Remove emacs ruby stuff that is like a decade old.

--- home/emacs/emacs.d/site-lisp/emacs_include.el
+++ home/emacs/emacs.d/site-lisp/emacs_include.el
@@ -30,21 +30,3 @@
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-;; Ruby
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
-(setq interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
-(add-hook 'ruby-mode-hook
-          (lambda()
-            (add-hook 'local-write-file-hooks
-                      '(lambda()
-                         (save-excursion
-                           (untabify (point-min) (point-max))
-                           (delete-trailing-whitespace)
-                           )))
-            (set (make-local-variable 'indent-tabs-mode) 'nil)
-            (set (make-local-variable 'tab-width) 2)
-            (imenu-add-to-menubar "IMENU")
-            (define-key ruby-mode-map "C-m" 'newline-and-indent) ; Not sure if this line is 100% right but it works!
-            (require 'ruby-electric)
-            (ruby-electric-mode t)
-            ))
