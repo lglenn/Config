6512cabcd000c3100b2f41fcf189fa02584516fa
Larry Glenn <yrral.glenn@gmail.com>
2017-12-05 18:29:45 -0500

    cleanup

--- home/emacs/emacs.d/site-lisp/my-org-config.el
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -11,7 +11,15 @@
 (setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@coop" . ?c)))
 (setq org-tag-persistent-alist org-tag-alist)
 (setq org-todo-keywords
-      '((sequence "TODO (t!)" "PLAN (p!)" "NEXT-ACTION (n!)" "WAITING (w@/!)" "APPT (a!)" "DEFERRED (d!)" "|" "CANCELLED (c@)" "DONE (D!)")))
+      '((sequence "TODO (t!)"
+		  "PLAN (p!)"
+		  "NEXT-ACTION (n!)"
+		  "WAITING (w@/!)"
+		  "APPT (a!)"
+		  "DEFERRED (d!)"
+		  "|"
+		  "CANCELLED (c@)"
+		  "DONE (D!)")))
 
 (setq org-log-done 'time)
 
