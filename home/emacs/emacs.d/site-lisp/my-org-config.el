f12ffac0844de7a6429af0f09c1867363a91d0ca
Larry Glenn <yrral.glenn@gmail.com>
2017-12-05 17:12:57 -0500

    Move emacs org-mode config from init.el to site_lisp/my-org-config.el

---
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -0,0 +1,53 @@
+;;; Org-mode
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@coop" . ?c)))
+(setq org-tag-persistent-alist org-tag-alist)
+(setq org-todo-keywords
+      '((sequence "TODO (t!)" "PLAN (p!)" "NEXT-ACTION (n!)" "WAITING (w@/!)" "APPT (a!)" "DEFERRED (d!)" "|" "CANCELLED (c@)" "DONE (D!)")))
+
+(setq org-log-done 'time)
+
+;; Default location for org files
+(setq org-directory "~/SpiderOak Hive/gtd/")
+(setq org-gtd-inbox-file (concat org-directory "inbox.org"))
+(setq org-gtd-tickler-file (concat org-directory "tickler.org"))
+
+;; Mobile Org Config
+;; See: http://orgmode.org/manual/MobileOrg.html#MobileOrg
+
+;; Mobile files are exchanged here.
+(setq org-mobile-directory "~/Dropbox (Personal)/Apps/MobileOrg")
+
+;; By default, files in org-agenda-files are included. Add other files by setting
+;; org-mobile-files
+
+(setq org-mobile-inbox-for-pull (concat org-directory "mobile-inbox.org"))
+
+;; Ideas from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
+(setq org-agenda-files (mapcar (lambda (e) (concat org-directory e)) '("inbox.org"
+								       "mobile-inbox.org"
+								       "gtd.org"
+								       "tickler.org")))
+
+;; Log todo state changes
+(setq org-log-into-drawer "LOGBOOK")
+
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                              (file+headline org-gtd-inbox-file "Tasks")
+                               "* TODO %i%?")
+                              ("T" "Tickler" entry
+                               (file+headline org-gtd-tickler-file "Tickler")
+                               "* %i%? \n %U")))
+
+(setq org-default-notes-file (concat org-directory "inbox.org"))
+     (define-key global-map "\C-cc" 'org-capture)
+
+(setq org-refile-targets (mapcar (lambda (e) (cons (concat org-directory (car e)) (cdr e)))
+      '(
+	("gtd.org" :maxlevel . 3)
+      	("someday.org" :level . 1)
+      	("tickler.org" :maxlevel . 2)
+	)))
+
+(provide 'my-org-config)
