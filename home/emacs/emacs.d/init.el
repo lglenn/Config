d9ff1867809ba44ae6340a980533ff4befd803ca
Larry Glenn <yrral.glenn@gmail.com>
2017-12-18 16:29:04 -0500

    Use Ivy instead of Ido for completion

--- home/emacs/emacs.d/init.el
+++ home/emacs/emacs.d/init.el
@@ -41,9 +41,18 @@
 ;; Dedupe command history
 (setq history-delete-duplicates t)
 
-;; Interactively Do Things (highly recommended, but not strictly required)
-(require 'ido)
-(ido-mode t)
+;; Ivy for completion
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 
 ;;; Git
 (require 'git)
