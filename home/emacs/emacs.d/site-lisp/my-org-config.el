7901a197030b1e06dead853e5b76e197ec5a107f
Larry Glenn <yrral.glenn@gmail.com>
2019-10-10 16:24:56 -0400

    Whitespace cleanup in org mode init file

--- home/emacs/emacs.d/site-lisp/my-org-config.el
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -23,27 +23,27 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 (setq org-tag-alist '(
-		      ("@work" . ?w)
-		      ("@home" . ?h)
-		      ("@laptop" . ?l)
-		      ))
-      
+                      ("@work" . ?w)
+                      ("@home" . ?h)
+                      ("@laptop" . ?l)
+                      ))
+
 (setq org-tag-persistent-alist org-tag-alist)
 (setq org-todo-keywords
       '((sequence "TODO(t!)"
-		  "PLAN(p!)"
-		  "WAITING(w@/!)"
-		  "APPT(a!)"
-		  "DEFERRED(d!)"
-		  "DELEGATED(l@)"
-		  "SCHEDULED(s!)"
-		  "|"
-		  "CANCELLED(c@)"
-		  "EXPIRED(e!)"
-		  "GARBAGE(g)"
-		  "NOT ACTIONABLE(n)"
-		  "DUPE(u!)"
-		  "DONE(D!)")))
+                  "PLAN(p!)"
+                  "WAITING(w@/!)"
+                  "APPT(a!)"
+                  "DEFERRED(d!)"
+                  "DELEGATED(l@)"
+                  "SCHEDULED(s!)"
+                  "|"
+                  "CANCELLED(c@)"
+                  "EXPIRED(e!)"
+                  "GARBAGE(g)"
+                  "NOT ACTIONABLE(n)"
+                  "DUPE(u!)"
+                  "DONE(D!)")))
 
 (setq org-log-done 'time)
 
@@ -76,26 +76,26 @@
 
 ;; Ideas from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
 (setq org-agenda-files (list
-			org-gtd-tickler-file
-			org-gtd-tasks-file
-			org-gtd-mobile-inbox-file))
+                         org-gtd-tickler-file
+                         org-gtd-tasks-file
+                         org-gtd-mobile-inbox-file))
 
 ;; Custom Agenda Views
 
 (defun skip-no-priority ()
   (and
-   (not
-    (member (org-entry-get nil "PRIORITY") '("A" "C")))
-   (point-at-eol)))
-      
+    (not
+      (member (org-entry-get nil "PRIORITY") '("A" "C")))
+    (point-at-eol)))
+
 (setq org-agenda-custom-commands
       '(("n" "Agenda and all TODOs" ((agenda "") (alltodo "")))
-	("p" "Agenda for items with non-default priority (A or C)" ((agenda "") (alltodo "")) ((org-agenda-skip-function 'skip-no-priority)))
-	("x" agenda)
+        ("p" "Agenda for items with non-default priority (A or C)" ((agenda "") (alltodo "")) ((org-agenda-skip-function 'skip-no-priority)))
+        ("x" agenda)
         ("y" agenda*)
         ("w" todo "WAITING|DELEGATED")
-	("@" . "Special tag searches: @c: @coop @h: @home @s: @ssi @t: @techpanel @w: @work")
-	("@s" tags "+@ssi")
+        ("@" . "Special tag searches: @c: @coop @h: @home @s: @ssi @t: @techpanel @w: @work")
+        ("@s" tags "+@ssi")
         ("@w" tags "+@work")
         ("@h" tags "+@home")
         ("@c" tags "+@coop")
@@ -105,33 +105,33 @@
 (setq org-log-into-drawer "LOGBOOK")
 
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
-			       (file+headline org-gtd-inbox-file "Inbox")
-			       "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
-			      ("p" "Todo [projects]" entry
-			       (file+headline org-gtd-tasks-file "Projects")
-			       "* %^{Brief Description} [/] %^g\n%?\nAdded: %U")
-			      ("m" "Meeting" entry (file+headline meeting-notes-file "Meetings")
-			       "* %^{Description}\n** Date: %^U\n** Attendees\n   - \n** Notes\n   - %?" :empty-lines 1)
-			      ("i" "Interview" entry (file+headline interviews-file "Interviews")
-			       "* %^{Candidate Name}\n** Date: %^U\n** Notes\n  - %?" :empty-lines 1)
-			      ("f" "Feedback" entry (file+headline feedback-file "Feedback")
-			       "* %^{Person}\n:PROPERTIES:\n:person: %\\1\n:END:\n** Date: %^U\n** Feedback\n*** Situation\n  %?\n*** Behavior\n*** Impact" :empty-lines 1)
-			      ("d" "Draft" entry (file+headline drafts-file "Drafts")
-			       "* %^{Subject}\n** Date: %^U\n** Notes\n%?" :empty-lines 1)
-            ("g" "Glossary" entry (file+headline glossary-file "Glossary")
-			       "** %^{Term}\n:PROPERTIES:\n:term %\\1\n:END:\n %?" :empty-lines 1)
-			      ("T" "Tickler" entry
+                               (file+headline org-gtd-inbox-file "Inbox")
+                               "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
+                              ("p" "Todo [projects]" entry
+                               (file+headline org-gtd-tasks-file "Projects")
+                               "* %^{Brief Description} [/] %^g\n%?\nAdded: %U")
+                              ("m" "Meeting" entry (file+headline meeting-notes-file "Meetings")
+                               "* %^{Description}\n** Date: %^U\n** Attendees\n   - \n** Notes\n   - %?" :empty-lines 1)
+                              ("i" "Interview" entry (file+headline interviews-file "Interviews")
+                               "* %^{Candidate Name}\n** Date: %^U\n** Notes\n  - %?" :empty-lines 1)
+                              ("f" "Feedback" entry (file+headline feedback-file "Feedback")
+                               "* %^{Person}\n:PROPERTIES:\n:person: %\\1\n:END:\n** Date: %^U\n** Feedback\n*** Situation\n  %?\n*** Behavior\n*** Impact" :empty-lines 1)
+                              ("d" "Draft" entry (file+headline drafts-file "Drafts")
+                               "* %^{Subject}\n** Date: %^U\n** Notes\n%?" :empty-lines 1)
+                              ("g" "Glossary" entry (file+headline glossary-file "Glossary")
+                               "** %^{Term}\n:PROPERTIES:\n:term %\\1\n:END:\n %?" :empty-lines 1)
+                              ("T" "Tickler" entry
                                (file+headline org-gtd-tickler-file "Tickler")
                                "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")))
 
 (setq org-default-notes-file org-gtd-inbox-file)
 
 (setq org-refile-targets (mapcar (lambda (e) (cons (concat org-directory (car e)) (cdr e)))
-      '(
-	("gtd.org" :maxlevel . 3)
-      	("ancillary/someday.org" :level . 1)
-      	("ancillary/tickler.org" :maxlevel . 2)
-	("ancillary/inbox.org" :maxlevel . 2)
-	)))
+                                 '(
+                                   ("gtd.org" :maxlevel . 3)
+                                   ("ancillary/someday.org" :level . 1)
+                                   ("ancillary/tickler.org" :maxlevel . 2)
+                                   ("ancillary/inbox.org" :maxlevel . 2)
+                                   )))
 
 (provide 'my-org-config)
