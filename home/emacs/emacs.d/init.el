947b9848da8bfe6058f938c50faadced47fdae41
Larry Glenn <yrral.glenn@gmail.com>
2020-01-24 08:39:47 +0000

    Add doom themes to emacs

--- home/emacs/emacs.d/init.el
+++ home/emacs/emacs.d/init.el
@@ -126,3 +126,31 @@
 	  (favefile key-combo filename (if readonly 'find-file-read-only 'find-file)))) faves))
 
 (favefiles favorite-files)
+
+;;; Stolen from http://www.mycpu.org/emacs-productivity-setup/
+
+(require 'doom-themes)
+
+(require 'indent-guide)
+(indent-guide-global-mode)
+(set-face-background 'indent-guide-face "dimgray")
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
+;; theme may have their own settings.
+(load-theme 'doom-molokai t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme
+; all-the-icons fonts must be installed!
+; do do this: M-x all-the-icons-install-fonts
+(doom-themes-neotree-config)  
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+
