68905d9fe055c57ca3453c51ba4986c5c2b70008
Larry Glenn <yrral.glenn@gmail.com>
2020-02-29 08:51:13 -0500

    Whitespace change from moving to emacs indentation

--- home/emacs/emacs.d/site-lisp/my-org-config.el
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -28,47 +28,47 @@
 (setq org-hide-emphasis-markers t)
 
 (let* ((variable-tuple
-         (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-               ((x-list-fonts "Verdana")         '(:font "Verdana"))
-               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-               (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+        (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
        (base-font-color     (face-foreground 'default nil 'default))
        (headline           `(:inherit default :weight normal :foreground ,base-font-color)))
 
   (custom-theme-set-faces
-    'user
-    `(org-level-8 ((t (,@headline ,@variable-tuple))))
-    `(org-level-7 ((t (,@headline ,@variable-tuple))))
-    `(org-level-6 ((t (,@headline ,@variable-tuple))))
-    `(org-level-5 ((t (,@headline ,@variable-tuple))))
-    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1))))
-    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.1))))
-    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.15))))
-    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.2))))
-    `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+   'user
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.15))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.2))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
 
 (custom-theme-set-faces
-  'user
-  '(variable-pitch ((t (:family "Source Sans Pro" :height 1.1 :weight normal))))
-  '(fixed-pitch ((t ( :family "Inconsolata" :slant normal :weight medium :height 1.0 :width normal)))))
+ 'user
+ '(variable-pitch ((t (:family "Source Sans Pro" :height 1.1 :weight normal))))
+ '(fixed-pitch ((t ( :family "Inconsolata" :slant normal :weight medium :height 1.0 :width normal)))))
 
 (add-hook 'org-mode-hook 'visual-line-mode)
 
 (custom-theme-set-faces
-  'user
-  '(org-block ((t (:inherit fixed-pitch))))
-  '(org-code ((t (:inherit (shadow fixed-pitch)))))
-  '(org-document-info ((t (:foreground "dark orange"))))
-  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
-  '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
-  '(org-link ((t (:foreground "royal blue" :underline t))))
-  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-  '(org-property-value ((t (:inherit fixed-pitch))) t)
-  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-  '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
-  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
-  '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+ 'user
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 
 ;; Global keys
 (global-set-key "\C-cl" 'org-store-link)
@@ -119,16 +119,16 @@
 
 ;; Ideas from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
 (setq org-agenda-files (list
-                         org-gtd-tickler-file
-                         org-gtd-tasks-file))
+                        org-gtd-tickler-file
+                        org-gtd-tasks-file))
 
 ;; Custom Agenda Views
 
 (defun skip-no-priority ()
   (and
-    (not
-      (member (org-entry-get nil "PRIORITY") '("A" "C")))
-    (point-at-eol)))
+   (not
+    (member (org-entry-get nil "PRIORITY") '("A" "C")))
+   (point-at-eol)))
 
 (setq org-agenda-custom-commands
       '(("n" "Agenda and all TODOs" ((agenda "") (alltodo "")))
@@ -179,10 +179,10 @@
                                    ("tickler.org" :maxlevel . 2)
                                    ("inbox.org" :maxlevel . 2)))
             (prepend-directory-if-string
-              (lambda (e) 
-                (if (stringp (car e))
-                  (cons (concat org-directory (car e)) (cdr e))
-                  e))))
+             (lambda (e) 
+               (if (stringp (car e))
+                   (cons (concat org-directory (car e)) (cdr e))
+                 e))))
         (mapcar prepend-directory-if-string refile-target-files)))
 
 (provide 'my-org-config)
