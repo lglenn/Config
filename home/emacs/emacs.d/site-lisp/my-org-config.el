149ac88b9825794daf3f8ef6a9853704695890ce
Larry Glenn <yrral.glenn@gmail.com>
2020-03-02 09:34:45 -0500

    Refactor org config
    Move filename declarations to let bindings

--- home/emacs/emacs.d/site-lisp/my-org-config.el
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -102,89 +102,89 @@
 
 (setq org-log-done 'time)
 
-;; Default location for org files
-(setq org-directory "~/gtd/")
-(setq org-gtd-inbox-file (concat org-directory "inbox.org"))
-(setq org-gtd-tickler-file (concat org-directory "tickler.org"))
-(setq org-gtd-tasks-file (concat org-directory "gtd.org"))
-(setq meeting-notes-file "~/jet/MeetingNotes/meetings.org")
-(setq interviews-file "~/jet/People/Interviews/interviews.org")
-(setq feedback-file "~/jet/People/Feedback/feedback.org")
-(setq drafts-file "~/jet/Drafts/drafts.org")
-(setq glossary-file (concat org-directory "glossary.org"))
-(setq coach-file (concat org-directory "coach.org"))
+(let
+    ((org-directory "~/gtd/")
+     (org-gtd-inbox-file (concat org-directory "inbox.org"))
+     (org-gtd-tickler-file (concat org-directory "tickler.org"))
+     (org-gtd-tasks-file (concat org-directory "gtd.org"))
+     (meeting-notes-file "~/jet/MeetingNotes/meetings.org")
+     (interviews-file "~/jet/People/Interviews/interviews.org")
+     (feedback-file "~/jet/People/Feedback/feedback.org")
+     (drafts-file "~/jet/Drafts/drafts.org")
+     (glossary-file (concat org-directory "glossary.org"))
+     (coach-file (concat org-directory "coach.org"))
+     (refile-targets '((nil :level . 1)
+                       ("gtd.org" :maxlevel . 3)
+                       ("someday.org" :level . 1)
+                       ("tickler.org" :maxlevel . 2)
+                       ("inbox.org" :maxlevel . 2))))
 
-;; Stick archive files in their own directory
-(setq org-archive-location "./archive/archive.org::* From %s")
+  ;; Stick archive files in their own directory
+  (setq org-archive-location "./archive/archive.org::* From %s")
 
-;; Ideas from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
-(setq org-agenda-files (list
-                        org-gtd-tickler-file
-                        org-gtd-tasks-file))
+  ;; Ideas from https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
+  (setq org-agenda-files (list
+                          org-gtd-tickler-file
+                          org-gtd-tasks-file))
 
-;; Custom Agenda Views
+  ;; Custom Agenda Views
 
-(defun skip-no-priority ()
-  (and
-   (not
-    (member (org-entry-get nil "PRIORITY") '("A" "C")))
-   (point-at-eol)))
+  (defun skip-no-priority ()
+    (and
+     (not
+      (member (org-entry-get nil "PRIORITY") '("A" "C")))
+     (point-at-eol)))
 
-(setq org-agenda-custom-commands
-      '(("n" "Agenda and all TODOs" ((agenda "") (alltodo "")))
-        ("p" "Agenda for items with non-default priority (A or C)" ((agenda "") (alltodo "")) ((org-agenda-skip-function 'skip-no-priority)))
-        ("x" agenda)
-        ("y" agenda*)
-        ("w" todo "WAITING|DELEGATED")
-        ("@" . "Special tag searches: @c: @coop @h: @home @s: @ssi @t: @techpanel @w: @work")
-        ("@s" tags "+@ssi")
-        ("@w" tags "+@work")
-        ("@h" tags "+@home")
-        ("@c" tags "+@coop")
-        ("@t" tags "+@techpanel")))
+  (setq org-agenda-custom-commands
+	'(("n" "Agenda and all TODOs" ((agenda "") (alltodo "")))
+          ("p" "Agenda for items with non-default priority (A or C)" ((agenda "") (alltodo "")) ((org-agenda-skip-function 'skip-no-priority)))
+          ("x" agenda)
+          ("y" agenda*)
+          ("w" todo "WAITING|DELEGATED")
+          ("@" . "Special tag searches: @c: @coop @h: @home @s: @ssi @t: @techpanel @w: @work")
+          ("@s" tags "+@ssi")
+          ("@w" tags "+@work")
+          ("@h" tags "+@home")
+          ("@c" tags "+@coop")
+          ("@t" tags "+@techpanel")))
 
-;; Log todo state changes
-(setq org-log-into-drawer "LOGBOOK")
+  ;; Log todo state changes
+  (setq org-log-into-drawer "LOGBOOK")
 
-(setq org-capture-templates '(("t" "Todo [inbox]" entry
-                               (file+headline org-gtd-inbox-file "Inbox")
-                               "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
-                              ("e" "Express Todo (straight to Tasks -- write well!)" entry
-                               (file+headline org-gtd-tasks-file "Tasks")
-                               "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
-                              ("p" "Todo [projects]" entry
-                               (file+headline org-gtd-tasks-file "Projects")
-                               "* %^{Brief Description} [/] %^g\n%?\nAdded: %U")
-                              ("m" "Meeting" entry (file+headline meeting-notes-file "Meetings")
-                               "* %^{Description}\n** Date: %^U\n** Attendees\n   - \n** Notes\n   - %?" :empty-lines 1)
-                              ("i" "Interview" entry (file+headline interviews-file "Interviews")
-                               "* %^{Candidate Name}\n** Date: %^U\n** Notes\n  - %?" :empty-lines 1)
-                              ("f" "Feedback" entry (file+headline feedback-file "Feedback")
-                               "* %^{Person}\n:PROPERTIES:\n:person: %\\1\n:END:\n** Date: %^U\n** Feedback\n*** Situation\n  %?\n*** Behavior\n*** Impact" :empty-lines 1)
-                              ("d" "Draft" entry (file+headline drafts-file "Drafts")
-                               "* %^{Subject}\n** Date: %^U\n** Notes\n%?" :empty-lines 1)
-                              ("g" "Glossary" entry (file+headline glossary-file "Glossary")
-                               "** %^{Term}\n:PROPERTIES:\n:term %\\1\n:END:\n %?" :empty-lines 1)
-                              ("c" "Coaching Observation" entry (file+headline coach-file "Capture") "** %?\n")
-                              ("T" "Tickler" entry
-                               (file+headline org-gtd-tickler-file "Tickler")
-                               "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")))
+  (setq org-capture-templates '(("t" "Todo [inbox]" entry
+				 (file+headline org-gtd-inbox-file "Inbox")
+				 "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
+				("e" "Express Todo (straight to Tasks -- write well!)" entry
+				 (file+headline org-gtd-tasks-file "Tasks")
+				 "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")
+				("p" "Todo [projects]" entry
+				 (file+headline org-gtd-tasks-file "Projects")
+				 "* %^{Brief Description} [/] %^g\n%?\nAdded: %U")
+				("m" "Meeting" entry (file+headline meeting-notes-file "Meetings")
+				 "* %^{Description}\n** Date: %^U\n** Attendees\n   - \n** Notes\n   - %?" :empty-lines 1)
+				("i" "Interview" entry (file+headline interviews-file "Interviews")
+				 "* %^{Candidate Name}\n** Date: %^U\n** Notes\n  - %?" :empty-lines 1)
+				("f" "Feedback" entry (file+headline feedback-file "Feedback")
+				 "* %^{Person}\n:PROPERTIES:\n:person: %\\1\n:END:\n** Date: %^U\n** Feedback\n*** Situation\n  %?\n*** Behavior\n*** Impact" :empty-lines 1)
+				("d" "Draft" entry (file+headline drafts-file "Drafts")
+				 "* %^{Subject}\n** Date: %^U\n** Notes\n%?" :empty-lines 1)
+				("g" "Glossary" entry (file+headline glossary-file "Glossary")
+				 "** %^{Term}\n:PROPERTIES:\n:term %\\1\n:END:\n %?" :empty-lines 1)
+				("c" "Coaching Observation" entry (file+headline coach-file "Capture") "** %?\n")
+				("T" "Tickler" entry
+				 (file+headline org-gtd-tickler-file "Tickler")
+				 "* TODO %^{Brief Description} %^g\n%?\nAdded: %U")))
 
-(setq org-default-notes-file org-gtd-inbox-file)
+  (setq org-default-notes-file org-gtd-inbox-file)
 
-(setq org-refile-targets 
-      (let ((refile-targets '((nil :level . 1)
-                                   ("gtd.org" :maxlevel . 3)
-                                   ("someday.org" :level . 1)
-                                   ("tickler.org" :maxlevel . 2)
-                                   ("inbox.org" :maxlevel . 2)))
-            (prepend-directory-if-string
-             (lambda (target)
-	       (let ((file (car target)))
-		 (if (stringp file)
-		     (let ((path (concat org-directory file)))
-		       (cons path (cdr target)))
-                   target))))
-        (mapcar prepend-directory-if-string refile-targets)))
+  (setq org-refile-targets 
+	(let ((prepend-directory-if-string
+               (lambda (target)
+		 (let ((file (car target)))
+		   (if (stringp file)
+		       (let ((path (concat org-directory file)))
+			 (cons path (cdr target)))
+                     target)))))
+              (mapcar prepend-directory-if-string refile-targets))))
 
 (provide 'my-org-config)
