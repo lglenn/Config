dc8b7917a36e687bdc213644f9238ec1f893eebe
Larry Glenn <yrral.glenn@gmail.com>
2021-04-28 10:04:03 -0400

    Remove kb mapping for deprecated org-iswitchb function

--- home/emacs/emacs.d/site-lisp/my-org-config.el
+++ home/emacs/emacs.d/site-lisp/my-org-config.el
@@ -74,7 +74,6 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
 
 (setq org-tag-alist '(
                       ("@work" . ?w)
